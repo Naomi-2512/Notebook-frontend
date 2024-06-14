a029719c1bd9c41877188c8522e9e1fb57d942de
Naomi2512 <naomichege2512@gmail.com>
2024-06-14 18:50:41 +0300

    added email validation

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink} from '@angular/router'
@@ -5,7 +6,7 @@ import { RouterLink} from '@angular/router'
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [RouterLink,FormsModule],
+  imports: [RouterLink,FormsModule,CommonModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
